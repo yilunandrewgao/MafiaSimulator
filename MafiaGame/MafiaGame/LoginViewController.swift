08dfeea70a5f133d32ef00acfc8917a0d1f90901
Yufang Lin <lin15556@Yufangs-MacBook-Pro.local>
2017-02-17 10:08:07 -0800

    login view controller + class

--- MafiaGame/MafiaGame/LoginViewController.swift
+++ MafiaGame/MafiaGame/LoginViewController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MafiaGame
 //
 //  Created by Yufang Lin on 2/17/17.
 //  Copyright © 2017 Yufang Lin. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
