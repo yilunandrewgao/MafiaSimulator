10660604841ccc59f7b11c770f32771ec0d677f6
Yufang Lin <lin15556@Yufangs-MacBook-Pro.local>
2017-02-19 14:55:03 -0800

    forgot to add label in join room

---
+++ MafiaGame/MafiaGame/CreateRoomController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateRoomController.swift
+//  MafiaGame
+//
+//  Created by Yufang Lin on 2/19/17.
+//  Copyright © 2017 Yufang Lin. All rights reserved.
+//
+
+import UIKit
+
+class CreateRoomController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
