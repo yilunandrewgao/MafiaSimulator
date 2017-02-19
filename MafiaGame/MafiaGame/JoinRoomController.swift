b261fee11e0a12385fea654898cfd43fc8745aca
Yufang Lin <lin15556@Yufangs-MacBook-Pro.local>
2017-02-19 14:44:18 -0800

    join room

---
+++ MafiaGame/MafiaGame/JoinRoomController.swift
@@ -0,0 +1,41 @@
+//
+//  JoinRoomController.swift
+//  MafiaGame
+//
+//  Created by Yufang Lin on 2/19/17.
+//  Copyright © 2017 Yufang Lin. All rights reserved.
+//
+
+
+import UIKit
+
+class JoinRoomController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RoomCell", for: indexPath)
+        
+        cell.textLabel?.text = "Room Name and Owner"
+        cell.detailTextLabel?.text = "Status"
+        
+        return cell
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var roomTable: UITableView!
+    
+}
--- MafiaGame/MafiaGame/MenuViewController.swift
+++ MafiaGame/MafiaGame/MenuViewController.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2017 Yufang Lin. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
