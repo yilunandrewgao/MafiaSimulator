f5f235ef3603e0510a06fc919cebf230ad640a50
Yufang Lin <lin15556@128-223-223-194-wireless.uoregon.edu>
2017-02-20 09:07:08 -0800

    update

---
+++ MafiaGame/MafiaGame/MafiaNightController.swift
@@ -0,0 +1,26 @@
+//
+//  MafiaNightController.swift
+//  MafiaGame
+//
+//  Created by Yufang Lin on 2/20/17.
+//  Copyright © 2017 Yufang Lin. All rights reserved.
+//
+
+import UIKit
+
+class MafiaNightController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
