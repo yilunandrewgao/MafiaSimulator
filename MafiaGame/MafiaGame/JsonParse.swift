69a63f7ba7077d397cc7152219ad02db098bd6c0
yilungao <yilungao@gmail.com>
2017-03-10 10:00:56 -0800

    made json consistent

--- MafiaGame/MafiaGame/JsonParse.swift
+++ MafiaGame/MafiaGame/JsonParse.swift
@@ -33,34 +33,30 @@ extension Player {
 
 extension Room{
     
-    convenience init(dataJSON: Data) throws {
-        let json = try? JSONSerialization.jsonObject(with: dataJSON, options: [])
+    convenience init(json: [String:Any]) throws {
         
-        guard let roomDict = json as? [String:Any] else{
-            throw SerializationError.missing("JsonObject")
-        }
         
         //player list
-        guard let playerListJSON = roomDict["playerList"] as? [[String:Any]] else {
+        guard let playerListJSON = json["playerList"] as? [[String:Any]] else {
             throw SerializationError.missing("playerList")
         }
         var playerList: [Player] = []
         for playerJSON in playerListJSON {
-            let player = try Player(json: playerJSON)
+            let player = try Player(playerInfo: playerJSON)
             playerList.append(player)
         }
         
         
-        guard let roomName = roomDict["roomName"] as? String else{
+        guard let roomName = json["roomName"] as? String else{
             throw SerializationError.missing("roomName")
         }
-        guard let password = roomDict["password"] as? String else{
+        guard let password = json["password"] as? String else{
             throw SerializationError.missing("password")
         }
-        guard let maxPlayers = roomDict["maxPlayers"] as? Int else {
+        guard let maxPlayers = json["maxPlayers"] as? Int else {
             throw SerializationError.missing("maxPlayers")
         }
-        guard let ownerJSON = roomDict["owner"] as? [String:Any] else {
+        guard let ownerJSON = json["owner"] as? [String:Any] else {
             throw SerializationError.missing("owner")
         }
         
@@ -71,36 +67,30 @@ extension Room{
 }
     
 extension SimpleRoom{
-        convenience init(dataJSON: Data){
-            let json = try? JSONSerialization.jsonObject(with: dataJSON, options: [])
+    convenience init(json: [String:Any]) throws {
             
-            guard let simpleRoomDict = json as? [String:Any] else{
-                throw SerializationError.missing("simpleRoom")
-            }
-            
-            guard let currentNumPlayers = simpleRoomDic["currentNumPlayers"] as? Int else{
+            guard let currentNumPlayers = json["currentNumPlayers"] as? Int else{
                 throw SerializationError.missing("currentNumPlayers")
             }
             
-            guard let roomName = simpleRoomDict["roomName"] as? String else{
+            guard let roomName = json["roomName"] as? String else{
                 throw SerializationError.missing("roomName")
             }
             
-            guard let maxPlayers = simpleRoomDict["maxPlayers"] as? Int else {
+            guard let maxPlayers = json["maxPlayers"] as? Int else {
                 throw SerializationError.missing("maxPlayers")
             }
             
             //owner
-            guard let ownerJson = simpleRoomDict["owner"] as? String else{
-                throw Serialization.missing("owner")
+            guard let ownerName = json["owner"] as? String else{
+                throw SerializationError.missing("owner")
             }
-            let owner = try Player(playerInfo: ownerJson)
             
-            guard let password = simpleRoomDict["password"] as? String else{
+            guard let password = json["password"] as? String else{
                 throw SerializationError.missing("password")
             }
             
-            self.init(currentNumPlayers: currentNumPlayers, roomName: roomName, maxPlayers: maxPlayers, owner: owner, password: password)
+            self.init(numPlayers: currentNumPlayers, roomName: roomName, password: password, maxPlayers: maxPlayers, owner: ownerName)
         }
 }
   
--- MafiaGame/MafiaGame/SimpleRoom.swift
+++ MafiaGame/MafiaGame/SimpleRoom.swift
@@ -9,19 +9,6 @@
 import UIKit
 
 class SimpleRoom {
-    private(set) var currentNumPlayers: Int
-    private(set) var roomName: String
-    private(set) var maxPlayers : Int
-    private(set) var owner: Player
-    private(set) var password: String
-    
-    init(currentNumPlayers: Int, roomName: String, maxPlayers: Int, owner: Player, password: String) {
-        self.currentNumPlayers = currentNumPlayers
-        self.roomName = roomName
-        self.maxPlayers = maxPlayers
-        self.owner = owner
-        self.password = password
-    }
     
     private(set) var numPlayers: Int
     private(set) var roomName: String
