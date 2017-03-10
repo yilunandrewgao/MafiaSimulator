f297ce7e651d0d6fcdea35c158c55e8a4870798b
Yufang Lin <lin15556@128-223-223-194-wireless.uoregon.edu>
2017-03-10 10:43:17 -0800

    socket manager

--- MafiaGame/MafiaGame/AppDelegate.swift
+++ MafiaGame/MafiaGame/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SocketIOManager.shared.establishConnection()
         return true
     }
 
@@ -28,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        SocketIOManager.shared.closeConnection()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -41,8 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        HostManager.shared.advertiser.stopAdvertisingPeer()
-        self.saveContext()
+        
     }
 
     // MARK: - Core Data stack
--- MafiaGame/MafiaGame/SocketIOManager.swift
+++ MafiaGame/MafiaGame/SocketIOManager.swift
@@ -8,5 +8,35 @@
 
 import UIKit
 class SocketIOManager: NSObject {
+    static let shared = SocketIOManager()
+    
+    private override init(){
+        super.init()
+    }
+    
+    var socket: SocketIOClient = SocketIOClient(socketURL: URL(string: "http://10.111.194.65:7777")!)
+    
+    func establishConnection(){
+        socket.connect()
+    }
+    
+    func closeConnection(){
+        socket.disconnect()
+    }
+    
+    
+    func userExitRoom(completionHandler: () -> Void) {
+        socket.emit("UserExitRoom")
+        socket.on("roomListUpdate") { (dataAray, ack) -> Void in
+            completionHandler(_ : dataArray[0] as? [[String: AnyObject]])
+        completionHandler()
+    }
+    
+    func userJoinRoom(completionHandler: () -> Void) {
+        socket.emit("UserJoinRoom")
+        
+        sompletionHandler()
+    }
+    
     
 }
