b20f856bf3520fc4b3e3aefccb79e0427e37f724
Yufang Lin <lin15556@128-223-223-194-wireless.uoregon.edu>
2017-03-10 08:07:07 -0800

    ove error

--- MafiaGame/MafiaGame/Room.swift
+++ MafiaGame/MafiaGame/Room.swift
@@ -10,27 +10,7 @@ import UIKit
 
 
 class Room {
-    
-<<<<<<< HEAD
-    let playerList: [Player]
-    let roomName: String
-    let maxPlayers : Int
-    let owner: Player
-    
-    //json object
-    
-    
-    
-    init() {
-        ///////fake json object
-        //fake dictionary
-        var infoDict = ["playerList": [["name":"bob", "sid":"1"], ["name":"mike", "sid":"1"]], "roomName":"test", "password":"testPassword", "maxPlayer": 5, "owner": ["name": "bob", "sid" : "1"]]
-        
-        self.playerList = createPlayerList()
-        self.roomName = roomName
-        self.owner = owner
-        self.maxPlayers = maxPlayers
-=======
+
     private(set) var playerList: [Player]
     private(set) var roomName: String
     private(set) var password: String
@@ -43,7 +23,7 @@ class Room {
         self.password = password
         self.maxPlayers = maxPlayers
         self.owner = owner
->>>>>>> 142d4c60b62ded216dddf48f3ad3a15b97e9cec9
+
     }
     
     
