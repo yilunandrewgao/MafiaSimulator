19caec35e210888fd59d60a847335dcfc6a96d67
Yufang Lin <lin15556@128-223-223-194-wireless.uoregon.edu>
2017-03-14 13:04:24 -0700

    Mafia/Villager vote controller + controller

---
+++ MafiaGame/MafiaGame/VillagerDayController.swift
@@ -0,0 +1,24 @@
+//
+//  VillageDayController.swift
+//  MafiaGame
+//
+//  Created by Yufang Lin on 3/14/17.
+//  Copyright © 2017 Yufang Lin. All rights reserved.
+//
+
+import UIKit
+
+class VillagerDayController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ MafiaGame/MafiaGame/VillagerVoteController.swift
@@ -0,0 +1,24 @@
+//
+//  VillagerVoteController.swift
+//  MafiaGame
+//
+//  Created by Yufang Lin on 3/14/17.
+//  Copyright © 2017 Yufang Lin. All rights reserved.
+//
+
+import UIKit
+
+class VillagerVoteController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
