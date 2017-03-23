7b499db43fdeb7a9595f80a1e705c014dda02bc3
Yufang Lin <lin15556@128-223-223-67-wireless.uoregon.edu>
2017-03-22 19:50:49 -0700

    profile view controller

---
+++ MafiaGame/MafiaGame/ProfileController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileController.swift
+//  MafiaGame
+//
+//  Created by Yufang Lin on 3/22/17.
+//  Copyright © 2017 Yufang Lin. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+       
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var roomCountLabel: UILabel!
+    
+    @IBOutlet weak var wonCountLabel: UILabel!
+    
+    @IBOutlet weak var lostCountLabel: UILabel!
+    
+}
