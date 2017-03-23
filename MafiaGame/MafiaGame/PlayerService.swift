cf2aaa487f91f1e52060ae9f40796ae8d4b9f56c
Yufang Lin <lin15556@128-223-223-67-wireless.uoregon.edu>
2017-03-22 18:11:37 -0700

    playerservice update

--- MafiaGame/MafiaGame/PlayerService.swift
+++ MafiaGame/MafiaGame/PlayerService.swift
@@ -11,19 +11,19 @@ import CoreData
 class PlayerService{
     
     func playerCategory() -> NSFetchedResultsController<PlayerInfo> {
-        let fetchRequest: NSFetchRequest<PlayerInfo> = PlayerInfo.fetchRequest() as! NSFetchRequest<PlayerInfo>
+        let fetchRequest: NSFetchRequest<PlayerInfo> = PlayerInfo.fetchRequest()
         
         return createFetchedResultsController(for: fetchRequest)
     }
     
     func rooms() -> NSFetchedResultsController<Rooms> {
-        let fetchRequest: NSFetchRequest<Rooms> = Rooms.fetchRequest() as! NSFetchRequest<Rooms>
+        let fetchRequest: NSFetchRequest<Rooms> = Rooms.fetchRequest()
         
         return createFetchedResultsController(for: fetchRequest)
     }
     
     func gameData() -> NSFetchedResultsController<GameData> {
-        let fetchRequest: NSFetchRequest<GameData> = GameData.fetchRequest() as! NSFetchRequest<GameData>
+        let fetchRequest: NSFetchRequest<GameData> = GameData.fetchRequest()
         
         return createFetchedResultsController(for: fetchRequest)
     }
@@ -42,14 +42,70 @@ class PlayerService{
     }
     
     
+    // Mark: Private set PlayerInfo name
+    private func nameSet(for username: String) {
+        let playerInfo = self.playerCategory().object(at: IndexPath(row: 0, section: 0))
+        playerInfo.setValue(username, forKey: "name")
+        
+    }
+    
+    // Mark: Private set Room hosted
+    private func hostedSet(for hostedCount: Int) {
+        let rooms = self.rooms().object(at: IndexPath(row: 0, section: 0))
+        rooms.setValue(hostedCount, forKey: "hosted")
+        
+    }
+    // Mark: Private set GameData won
+    private func wonSet(for wonCount: Int) {
+        let gameData = self.gameData().object(at: IndexPath(row: 0, section: 0))
+        gameData.setValue(wonCount, forKey: "won")
+    }
+    
+    // Mark: Private set GameData lost
+    private func lostSet(for lostCount: Int) {
+        let gameData = self.gameData().object(at: IndexPath(row: 0, section: 0))
+        gameData.setValue(lostCount, forKey: "lost")
+    }
+    
+    
     // MARK: Private init
     private init() {
+        
         persistentContainer = NSPersistentContainer(name: "MafiaGame")
         persistentContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
             let context = self.persistentContainer.viewContext
             
             context.perform {
-                //get username
+                //guard statement to check if any player Info objects have already been created
+                let fetchRequest: NSFetchRequest<PlayerInfo> = PlayerInfo.fetchRequest()
+                let count = (try? context.count(for: fetchRequest)) ?? 0
+                
+                guard count == 0 else {
+                    return
+                }
+                
+                //If there is no core data of player than below is inital data
+                
+                //PlayerInfo NSManagedObject
+                let playerInfo = PlayerInfo(context: context)
+                playerInfo.name = ""
+                
+                //Rooms NSManagedObject
+                let rooms = Rooms(context: context)
+                rooms.hosted = 0
+                
+                //GameData NSManagedObject
+                let gameData = GameData(context: context)
+                gameData.won = 0
+                gameData.lost = 0
+                
+                do {
+                    try context.save()
+                }
+                
+                catch _ {
+                    fatalError("Failed to save context after inserting initial player info data")
+                }
                 
             }
         })
